20d42041a34315370674eb3fd71ed692f0e33a77
ShobikaM <Shobika9696@gmail.com>
2021-12-15 05:53:19 +0530

    added 3 prints in both .pl & .py file

--- index.pl
+++ index.pl
@@ -1,1 +1,8 @@
-print ("Hi form Perl!!")
+#!/usr/bin/perl
+print ("Hi form Perl!!\n");
+my $name = "Shobika";
+my $age = 25;
+my $major = "Engineering";
+print "NAME ---> $name\n";
+print "AGE ---> $age\n";
+print "MAJOR ---> $major\n";
--- index.py
+++ index.py
@@ -1,2 +1,7 @@
 print("Hello!")
-print("all things are settled for python!!!!")
+#print("all things are settled for python!!!!")
+
+name = "Shobika"
+age = 25
+major = "Engineering"
+print("Name is {}, Age is {}, Mjor is {}".format(name,age,major))
